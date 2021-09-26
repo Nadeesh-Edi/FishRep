d6a4a69f37d6dcba7ced4651d7a1f3175ecb6445
IT20146238 <79472177+IT20146238@users.noreply.github.com>
2021-09-26 13:05:13 +0530

    Customer buy

---
+++ app/src/main/java/customer/Customer.java
@@ -0,0 +1,62 @@
+package customer;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String province;
+    private Integer phone;
+    private Integer packet;
+    private Integer quantity;
+
+    public Customer(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Integer getPacket() {
+        return packet;
+    }
+
+    public void setPacket(Integer packet) {
+        this.packet = packet;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ app/src/main/java/customer/ListAdapter.java
@@ -0,0 +1,53 @@
+package customer;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ListAdapter extends ArrayAdapter {
+
+    private final Activity mContext;
+    List<Customer> customerList;
+
+    public ListAdapter(Activity mContext, List<Customer> customerList) {
+        super(mContext,R.layout.list_item,customerList);
+        this.mContext = mContext;
+        this.customerList = customerList;
+    }
+
+    //    public ListAdapterBuy(Activity mContext, List<Customer> customerList);
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = mContext.getLayoutInflater();
+        View listItemView = inflater.inflate(R.layout.list_item, null, true);
+
+        TextView tvName = listItemView.findViewById(R.id.tvName);
+        TextView tvAddress = listItemView.findViewById(R.id.tvAddress);
+        TextView tvProvince = listItemView.findViewById(R.id.tvProvince);
+        TextView tvPhone = listItemView.findViewById(R.id.tvPhone);
+        TextView tvPacket = listItemView.findViewById(R.id.tvPacket);
+        TextView tvQty = listItemView.findViewById(R.id.tvQty);
+
+        Customer cus = customerList.get(position);
+
+        tvName.setText(cus.getName());
+        tvAddress.setText(cus.getAddress());
+        tvProvince.setText(cus.getProvince());
+        tvPhone.setText(cus.getPhone());
+        tvPacket.setText(cus.getPacket());
+        tvQty.setText(cus.getQuantity());
+
+        return listItemView;
+
+    }
+}
