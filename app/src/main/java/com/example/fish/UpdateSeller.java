0d0a94b59349a7f6f724763026173c30ab2e5fee
nadeesh9 <nadeeshedi@gmail.com>
2021-10-02 01:27:54 +0530

    Delivery new

--- app/src/main/java/com/example/fish/UpdateSeller.java
+++ app/src/main/java/com/example/fish/UpdateSeller.java
@@ -1,17 +1,20 @@
 package com.example.fish;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -47,24 +50,48 @@ public class UpdateSeller extends AppCompatActivity {
         Query query = FirebaseDatabase.getInstance().getReference().child("Seller")
                 .orderByChild("email").equalTo(email);
         query.addListenerForSingleValueEvent(valueEventListener);
-    }
 
-    public void updateUserSeller(View view) {
-        Query query = FirebaseDatabase.getInstance().getReference().child("Seller")
-                .orderByChild("email").equalTo(email);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.hasChildren()) {
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+            public void onClick(View view) {
+                Query query = FirebaseDatabase.getInstance().getReference().child("Seller")
+                        .orderByChild("email").equalTo(email);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.hasChildren()) {
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                dbRef = dataSnapshot.getRef();
+                                seller = dataSnapshot.getValue(Seller.class);
 
+                                if(TextUtils.isEmpty(name.getText().toString()))
+                                    Toast.makeText(getApplicationContext(), "Name cannot be empty", Toast.LENGTH_SHORT).show();
+                                else if(TextUtils.isEmpty(nic.getText().toString()))
+                                    Toast.makeText(getApplicationContext(), "NIC cannot be empty", Toast.LENGTH_SHORT).show();
+                                else if(TextUtils.isEmpty(phone.getText().toString()))
+                                    Toast.makeText(getApplicationContext(), "Phone number cannot be empty", Toast.LENGTH_SHORT).show();
+                                else {
+                                    seller.setName(name.getText().toString());
+                                    seller.setNic(nic.getText().toString());
+                                    seller.setNumber(phone.getText().toString());
+
+                                    dbRef.setValue(seller);
+                                    Toast.makeText(getApplicationContext(), "Account updated successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(UpdateSeller.this, MainActivity.class));
+                                }
+
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            }
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(), "Account update unsuccessful", Toast.LENGTH_SHORT).show();
                     }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getApplicationContext(), "Account cannot be updated", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
